1910fac33bd87cf3d755ec6def43ceb1e5638970
kuldeep.gautam <kuldeep.gautam@sisrtd.com>
2024-04-04 18:08:36 +0530

    Added config file

---
+++ vidly/src/config.js
@@ -0,0 +1,11 @@
+const config = {
+  commAging: 24, // in hours
+  localApi: false,
+  apiUrl: "/api",
+  // apiEndpoint: "http://localhost:8004",
+  apiEndpoint: "https://ip.sisrtd.com",
+  // apiEndpoint: "https://iprotect.sisrtd.com",
+  storageBucket: "https://files.sisrtd.com/iprotect",
+};
+
+export default config;
